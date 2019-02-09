32ffeaf97086787b1ee4c998f972c85e5c4f44de
Fethi Isfarca <fisfarca@googlemail.com>
2019-02-09 14:53:34 +0100

    Adding project.

---
+++ Client_Socket/Client_Socket/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace Client_Socket
+{
+    public partial class FormClient : Form
+    {
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+
+        public FormClient()
+        {
+            InitializeComponent();
+        }
+
+        private void FormClient_Load(object sender, EventArgs e)
+        {
+            msg("Client Started");
+            clientSocket.Connect("127.0.0.1", 8888);
+            label1.Text = "Client Socket Program - Server Connected ...";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NetworkStream serverStream = clientSocket.GetStream();
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(textBox2.Text + "$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            byte[] inStream = new byte[10025];
+            serverStream.Read(inStream, 0, inStream.Length);
+            string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+            msg(returndata);
+            textBox2.Text = "";
+            textBox2.Focus();
+        }
+
+        public void msg(string mesg)
+        {
+            textBox1.Text = textBox1.Text + Environment.NewLine + " >> " + mesg;
+        }
+    }
+}
---
+++ Server_Socket/Server_Socket/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Server_Socket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#pragma warning disable CS0618 // Type or member is obsolete
+            TcpListener serverSocket = new TcpListener(8888);
+#pragma warning restore CS0618 // Type or member is obsolete
+            int requestCount = 0;
+            TcpClient clientSocket = default(TcpClient);
+            serverSocket.Start();
+            Console.WriteLine(" >> Server Started");
+            clientSocket = serverSocket.AcceptTcpClient();
+            Console.WriteLine(" >> Accept connection from client");
+            requestCount = 0;
+
+            while ((true))
+            {
+                try
+                {
+                    requestCount = requestCount + 1;
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    byte[] bytesFrom = new byte[10025];
+                    networkStream.Read(bytesFrom, 0, bytesFrom.Length);
+                    string dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+                    Console.WriteLine(" >> Data from client - " + dataFromClient);
+                    string serverResponse = "Last Message from client - " + dataFromClient;
+                    Byte[] sendBytes = Encoding.ASCII.GetBytes(serverResponse);
+                    networkStream.Write(sendBytes, 0, sendBytes.Length);
+                    networkStream.Flush();
+                    Console.WriteLine(" >> " + serverResponse);
+                }
+                catch
+                {
+                    clientSocket.Close();
+                    serverSocket.Stop();
+                    Console.WriteLine(" >> exit");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
